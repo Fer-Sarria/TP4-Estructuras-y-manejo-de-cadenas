55ee117ee04e597641ea31e117b37936016c6ffa
Fer-Sarria <fer.cabj.007@gmail.com>
2026-07-30 12:49:53 -0300

    Agregar archivos de proyecto.

---
+++ TP4-Estructuras-y-manejo-de-cadenas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP4_Estructuras_y_manejo_de_cadenas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
